e9fb321c17de5bb05267162ed07b449c9d46fbdf
Francis Vaughan <francis.vaughan@ersa.edu.au>
2016-03-15 17:10:30 +1030

    more config fix

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'image_repo=Configuration:main',
+            'image_repo=src.Configuration:main',
         ],
     },
 )
