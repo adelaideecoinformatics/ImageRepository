b4ffd03008b4726de71cecffed71d4f63ae86a8c
Francis Vaughan <francis.vaughan@ersa.edu.au>
2016-03-15 15:52:59 +1030

    remove dateutil from dependancies to try to fix conflict with dataone

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['xmltodict','PyYAML','python-dateutil', 'Flask','swift','wand'],
+    install_requires=['xmltodict','PyYAML', 'Flask','swift','wand'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
