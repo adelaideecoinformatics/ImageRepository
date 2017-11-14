bc474842277eea95316be95761a9da2bd26ee0fe
Tom Saleeba <tom.saleeba@gmail.com>
2017-11-14 15:28:49 +1030

    docs: update error message when swift vars aren't supplied

--- docker/swift.sh
+++ docker/swift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo '[ERROR] you need to overwrite this file (/swift.sh) with a host file'
-echo 'Create a file on your host with the content:
+echo '[ERROR] you need to overwrite this file (/swift.sh) in the container with a host file'
+echo '[ERROR] Create a file on your host with the content:
 export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v2.0/
 export OS_USERNAME="user@edu.au"
 export OS_PASSWORD="pass"
@@ -9,5 +9,6 @@ export OS_TENANT_ID="id"
 export SWIFT_P="persistent-swift-container-name"
 export SWIFT_C="cache-swift-container-name"
 '
-echo 'Mount the host swift config with: docker -v /path/to/swift.sh:/swiftsh ...'
+echo '[ERROR] ...then edit the values in that file with your Swift credentials.'
+echo '[ERROR] Run the docker command again but mount the host swift config with: docker run -v /path/to/swift.sh:/swift.sh ...'
 exit 1
